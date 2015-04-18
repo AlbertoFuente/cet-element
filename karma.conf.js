139a0a36b0f4ea025380ecaa01078f6465ac6841
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-04-18 08:11:21 +0200

    Implemented PhantomJS to karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -59,7 +59,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome', 'Opera', 'Safari', 'Firefox'],
+    browsers: ['Chrome', 'Opera', 'Safari', 'Firefox', 'PhantomJS'],
 
 
     // Continuous Integration mode
